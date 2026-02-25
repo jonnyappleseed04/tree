d4430c290190a7e6cc706c8e2ee8ec8e3ff1fbac
jonathanfritz <jonnyappleseed04@gmail.com>
2026-02-25 09:56:29 -0800

    add sql file

---
+++ etl.py
@@ -0,0 +1,4 @@
+import pandas as pd
+#with open('Street_Tree_Inventory_-_Active_Records.csv') as tree_data:
+
+df = pd.read_csv('Street_Tree_Inventory_-_Active_Records.csv')
---
+++ tree-schema.sql
@@ -0,0 +1,147 @@
+/*
+tree database
+version 1.0
+date 2/23/2026
+authors: Amelia, Jonathan
+*/
+
+
+/*
+Regarding lengths of attributes--
+The database was put into a function that calculated the maximum # of characters for non-numeric columns, resulting:
+Neighborhood : 23
+Address : 59
+SPECIES : 79
+MATURE_SIZE : 7
+FUNCTIONAL_TYPE : 20
+Condition : 9
+Site_Type : 21
+Site_Size : 6
+Wires : 12
+SITE_IMPROVEMENT : 13.
+
+
+Other functions were used to acquire numerical maximums:
+X : 13 total, 4 after '.'
+Y : 11 total, 4 after '.'
+DIAMETER : 11 total, 9 after '.'
+Site_Width : 12 total, 9 after '.'
+
+
+OBJECTID : 6 digits (Automatically increasing)
+Date_Inventoried : Date
+
+
+If more data was intended to be collected, maximums would be increased
+*/
+
+
+drop database if exists tree;
+create database tree;
+use tree;
+
+
+create table neighborhood(
+neighborhood_name varchar(23),
+primary key (neighborhood_name)
+);
+
+
+-- plural table below:
+create table neighborhood_address(
+fk_neighborhood varchar(23),
+address varchar(59),
+primary key (fk_neighborhood, address),
+foreign key (fk_neighborhood) references neighborhood(neighborhood_name)
+  on update cascade
+  on delete cascade
+);
+
+
+-- normalization table below:
+create table location_type_width(
+width float(12,9),
+size varchar(6),
+primary key (width)
+);
+
+
+create table location_type(
+location_id int ,
+site_type varchar(21),
+width float(12,9),
+improvement varchar(13),
+wires varchar(12),
+primary key (location_id),
+foreign key (width) references location_type_width(width)
+  on update cascade
+  on delete cascade
+);
+
+
+create table coordinates(
+x float(13,4),
+y float(11,4),
+primary key (x, y),
+fk_neighborhood varchar(23),
+fk_location_type int not null, -- for many-to-one. not null bc location type is required.
+foreign key (fk_neighborhood) references neighborhood(neighborhood_name)
+  on update cascade
+  on delete cascade,
+foreign key (fk_location_type) references location_type(location_id)
+  on update cascade
+  on delete cascade
+);
+
+
+create table tree(
+object_id int auto_increment,
+date_inventoried date,
+fk_coordinates_x float(13,4),
+fk_coordinates_y float(11,4),
+primary key (object_id),
+foreign key (fk_coordinates_x, fk_coordinates_y) references coordinates(x, y)
+  on update cascade
+  on delete cascade
+);
+
+
+-- normalization table below:
+create table tree_details_species(
+species varchar(79),
+functional_type varchar(20),
+primary key (species)
+);
+
+
+create table tree_details(
+mature_size varchar(7),
+diameter float(11,9),
+item_condition varchar(9),
+species varchar(79),
+fk_tree int,
+primary key (fk_tree), -- SINGULAR weak table's foreign is also the primary key
+foreign key (fk_tree) references tree(object_id)
+  on update cascade
+  on delete cascade,
+foreign key (species) references tree_details_species (species)
+);
+
+
+create table fire_hydrant(
+object_id int auto_increment,
+primary key (object_id)
+);
+
+
+create table tree_is_next_to_fire_hydrant(
+fk_tree int,
+fk_fire_hydrant int,
+primary key (fk_tree, fk_fire_hydrant),
+foreign key (fk_tree) references tree(object_id)
+  on update cascade
+  on delete cascade,
+foreign key (fk_fire_hydrant) references fire_hydrant(object_id)
+  on update cascade
+  on delete cascade
+);
