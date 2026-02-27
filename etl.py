40dedde144b9164d5437130f4db283769fda962d
acwhitlock <amelia.c.whitlock@gmail.com>
2026-02-27 09:30:25 -0800

    -

--- etl.py
+++ etl.py
@@ -1,3 +1,19 @@
-import pandas as pd
+import csv
+def getDataset():
+    with open('../Downloads/Street_Tree_Inventory_-_Active_Records.csv') as f:
+        csv_reader = csv.reader(f, delimiter=',')
+        dataset=[]
+        for row in csv_reader:
+            dataset.append(row)
+    #print(len(dataset))
+    return dataset
+def main():
+    ds=getDataset()
+    values=[[] for _ in range(16)]
+    for row in ds:
+        for c in range(len(row)):
+            values[c].append(row[c])
+    print(values[0][1:11])
 
-df = pd.read_csv('Street_Tree_Inventory_-_Active_Records.csv')
+if __name__ == "__main__":
+    main()
