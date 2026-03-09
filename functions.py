c2669134659c7594e7f5a147e9904cc74e92d6fd
acwhitlock <amelia.c.whitlock@gmail.com>
2026-03-09 10:13:22 -0700

    -

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@ import pandas as pd
 import random
 #region
 def get_ran_value(min, max, count, null_prob=.5):
-    """Generates random number between min and max or null"""
+    """Returns a list with count entries of random numbers between min and max or null"""
     random.seed(42)
     random_list = random.sample(range(min, max), count)
     for i in range(len(random_list)):
