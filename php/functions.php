48597b4315e7528f421efee44447b51bfeff7e29
acwhitlock <amelia.c.whitlock@gmail.com>
2026-04-06 09:33:03 -0700

    functions to use for php assignment

---
+++ php/functions.php
@@ -0,0 +1,77 @@
+<?php
+    ini_set('display_errors',1);
+    ini_set('display_startup_errors',1);
+    error_reporting(E_ALL);
+
+    function db_connect() {
+        define('DB_USER', 'registrar');
+        define('DB_PASSWORD', '');
+        define('DB_HOST', 'localhost');
+        define('DB_NAME', 'registrar');
+
+        ($dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME))
+        || die('Could not connect to MariaDB: ' . mysqli_connect_error());
+        return $dbc;
+    }
+
+
+    function db_get_id_and_classes($dbc) {
+        $query = 'SELECT id, name FROM course group by name';
+        $response = mysqli_query($dbc, $query);
+        if ($response) {
+            $arr = [];
+            while ($row = mysqli_fetch_array($response)) {
+                array_push($arr, $row);
+            }
+            return $arr;
+        }
+        else {
+            die('Couldn\'t issue database query: ' . mysqli_error($dbc));
+        }
+    }
+
+    function db_get_students_from_class($dbc, $class_id){
+        $query =    'SELECT student.name FROM student 
+                    join student_takes_course on student.id = student_takes_course.student_id 
+                    where student_takes_course.course_id = ' . $class_id;
+        $response = mysqli_query($dbc, $query);
+        if ($response) {
+            $arr = [];
+            while ($row = mysqli_fetch_array($response)) {
+                array_push($arr, $row);
+            }
+            return $arr;
+        }
+        else {
+            die('Couldn\'t issue database query: ' . mysqli_error($dbc));
+        }
+
+    }
+
+    function db_get_class_name_from_id ($dbc, $c_id){
+        $query = 'SELECT name from course where id = '.$c_id;
+        $response = mysqli_query($dbc, $query);
+        if ($response) {
+            return $row = mysqli_fetch_array($response);
+        }
+        else {
+            die('Couldn\'t issue database query: ' . mysqli_error($dbc));
+        }
+    }
+    function db_get_class_names_from_string($dbc, $input){
+        $query =    'SELECT name FROM course
+                    where name like "%' . $input . '%";';
+        $response = mysqli_query($dbc, $query);
+        if ($response) {
+            $arr = [];
+            while ($row = mysqli_fetch_array($response)) {
+                array_push($arr, $row);
+            }
+            return $arr;
+        }
+        else {
+            die('Couldn\'t issue database query: ' . mysqli_error($dbc));
+        }
+
+    }
+?>
