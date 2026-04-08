1b23107436ea95c7df02c74171d82adabfacf415
jonathanfritz <jonnyappleseed04@gmail.com>
2026-04-07 20:45:46 -0700

    changes

--- php/search-results.php
+++ php/search-results.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -5,27 +6,48 @@
   </head>
   <body>
     <?php
-    $search_string = $_GET['search_string'];
+    ini_set('display_errors',1);
+    ini_set('display_startup_errors',1);
+    error_reporting(E_ALL);
+
+    $selection = $_GET['query'];
     include('functions.php');
     $dbc = db_connect();
-    $result = db_do_query($dbc,$search_string);
-  // if (count($result)==0){
-  //   echo 'No classes with "'.$search_string.'" were found.';
-  // }
-  // else{
-    echo '<table>';
-    echo '<tr><td><b>Name</b></td></tr>';
-    foreach ($result as $row) {
-      echo '<tr>';
+    //transform selection to query
+    $query = transform_selection($selection);
+    //other stuff
+    $result = db_do_query($dbc,$query);
 
-      for ($column = 0; $column<count($row)-2;$column +=1){
-        echo '<td>' . $row[$column] . '</td>';
+    echo '<p> Your query: ' . $query . '</p>';
+    echo '<p> press
+        <a href = index.html>this</a>
+        to go back </p>';
+    echo '<h2>'. 'results' . '</h2>';
+
+    //for data
+    //for update, deletes
+    if (is_int($result)){
+    echo '<p> Affected rows:'  . $result;}
+    else{
+    $first_row = $result[array_key_first($result)];
+    echo '<table>';
+      echo '<tr>'; //for column names
+        foreach (array_keys($first_row) as $column_name){
+          echo '<td>' . $column_name . '</td>';}
+      echo '</tr>';
+    foreach ($result as $row) {
+    //foreach is specific loop for arrays
+      echo '<tr>'; //table row
+
+      foreach ($row as $value){
+        echo '<td>' . $value . '</td>';
       }
       echo '</tr>';
     }
 
     echo '</table>';
-  // }    
+    }
+
 ?>
   </body>
 </html>
