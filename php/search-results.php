e220d3af147a959965d9d81b43cdbcf8343a9e21
jonathanfritz <jonnyappleseed04@gmail.com>
2026-04-10 14:30:29 -0700

    Small css stuf

--- php/search-results.php
+++ php/search-results.php
@@ -3,37 +3,41 @@
 <html>
   <head>
     <title>Results</title>
+    <link rel="stylesheet" href="styles.css">
   </head>
   <body>
     <?php
+    //error reporting
     ini_set('display_errors',1);
     ini_set('display_startup_errors',1);
     error_reporting(E_ALL);
 
-    $selection = $_GET['query'];
     include('functions.php');
     $dbc = db_connect();
+
+    $selection = $_GET['query'];
+
     //transform selection to query
     $query = transform_selection($selection);
-    //other stuff
     $result = db_do_query($dbc,$query);
 
-    echo '<p> Your query: ' . $query . '</p>';
-    echo '<p> press
-        <a href = index.html>this</a>
+    echo '<p>
+        <a href=index.html>&larr;</a>
         to go back </p>';
+    echo '<p> Your query: ' . $query . '</p>';
     echo '<h2>'. 'results' . '</h2>';
 
     //for data
     //for update, deletes
     if (is_int($result)){
     echo '<p> Affected rows:'  . $result;}
+    //for selects
     else{
     $first_row = $result[array_key_first($result)];
     echo '<table>';
       echo '<tr>'; //for column names
         foreach (array_keys($first_row) as $column_name){
-          echo '<td>' . $column_name . '</td>';}
+          echo '<th>' . $column_name . '</th>';}
       echo '</tr>';
     foreach ($result as $row) {
     //foreach is specific loop for arrays
